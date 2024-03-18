7444fa6b1d9b13a362bf0f3763d7e286b8ccf4aa
naseer12345 <109208526+naseer12345@users.noreply.github.com>
2024-03-18 10:37:56 +0000

    Update workExperienceTimeLine.tsx fixed the text color issue, changed the color form dark white to white

--- app/components/ui/workExperienceTimeLine.tsx
+++ app/components/ui/workExperienceTimeLine.tsx
@@ -8,7 +8,7 @@ function Timeline() {
       <li className="mb-10 ms-4">
         <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
         <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">June 2022 - July 2022</time>
-        <h3 className="text-lg font-semibold text-gray-900 dark:text-white">Software Developer Intern - Comma, now Weaver.io</h3>
+        <h3 className="text-lg font-semibold text-gray-900 text-white">Software Developer Intern - Comma, now Weaver.io</h3>
         <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">•	Worked with HTML, CSS, and learned backend development concepts.</p>
         <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">•	Developed an interest in web development technologies.</p>
 
@@ -16,14 +16,14 @@ function Timeline() {
       <li className="mb-10 ms-4">
         <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
         <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">December 2022 - May 2023</time>
-        <h3 className="text-lg font-semibold text-gray-900 dark:text-white">Web Developer (Contract)- Family Business </h3>
+        <h3 className="text-lg font-semibold text-gray-900 text-white">Web Developer (Contract)- Family Business </h3>
         <p className="text-base font-normal text-gray-500 dark:text-gray-400">•	Designed and developed a static website.</p>
         <p className="text-base font-normal text-gray-500 dark:text-gray-400">•	Managed social media presence and marketing initiatives.</p>
       </li>
       <li className="ms-4">
         <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
         <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">August 2023 - Present</time>
-        <h3 className="text-lg font-semibold text-gray-900 dark:text-white">.NET Developer - <i><a href="https://www.focus4people.com/">Focus4People</a></i>  </h3>
+        <h3 className="text-lg font-semibold text-gray-900 text-white">.NET Developer - <i><a href="https://www.focus4people.com/">Focus4People</a></i>  </h3>
         <p className="text-base font-normal text-gray-500 dark:text-gray-400">•	Built an automatic email sender using Mailbee and ASP.NET C++, facilitating weekly email campaigns to over 10,000 recipients.</p>
         <p className="text-base font-normal text-gray-500 dark:text-gray-400">•	Developed a custom AI using H2O.ai's open-source GPT tool, improving customer satisfaction through intelligent PDF data analysis.</p>
         <p className="text-base font-normal text-gray-500 dark:text-gray-400">•	Created a script for routine deletion of old backup data, optimizing storage and system performance.</p>
@@ -35,14 +35,14 @@ function Timeline() {
       <li className="mb-10 ms-4">
         <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
         <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">Current</time>
-        <h3 className="text-lg font-semibold text-gray-900 dark:text-white">BSc (Honours) Computing and IT (Software) @ open university</h3>
+        <h3 className="text-lg font-semibold text-gray-900 text-white">BSc (Honours) Computing and IT (Software) @ open university</h3>
 
        
       </li>
       <li className="mb-10 ms-4">
         <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
         <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">May 2023</time>
-        <h3 className="text-lg font-semibold text-gray-900 dark:text-white">GCSEs @ Ifield community college </h3>
+        <h3 className="text-lg font-semibold text-gray-900 text-white">GCSEs @ Ifield community college </h3>
         <p className="text-base font-normal text-gray-500 dark:text-gray-400">English(g5), Maths(g7), Computer Science(g7)</p>
       </li>
       
