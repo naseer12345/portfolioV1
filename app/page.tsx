79f30d0649e89ddb82a6864c11968fc16ecd267a
naseer12345 <109208526+naseer12345@users.noreply.github.com>
2024-03-19 08:29:09 +0000

    some alignments on page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -202,7 +202,7 @@ export default function Home() {
               
      
      
-     <div className="flex flex-col items-center justify-center mt-20 ">
+    <div className="flex flex-col items-center justify-center mt-20 ">
      
      <div className=" py-12">
   <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
